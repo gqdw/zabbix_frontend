59c0c27e2f25e4accc566c32aa1c1756588f16c4
gqdw <aca_jingru@qq.com>
2015-09-16 14:44:57 +0800

    add proxy support

--- api.py
+++ api.py
@@ -4,12 +4,24 @@
 import urllib2
 import json
 import sys
+
+class Proxy:
+	def __init__(self,proxyid,host,status):
+		self.proxyid = proxyid
+		self.host = host
+		self.status = status
+	def say(self):
+		print '%s %s %s' % (self.proxyid ,self.host ,self.status)
+		
+
+
 class ZabbixApi:
 	def __init__(self,api_user,api_pass):
 		self.zabbix_url = "http://zabbix.jiagouyun.com/api_jsonrpc.php"
 		self.api_user = api_user
 		self.api_pass = api_pass
 		self.id = 0
+		self.proxylist = []
 	
 	def auth(self):
 		auth_data={ 'jsonrpc':'2.0','method':'user.login','params':{'user':self.api_user,'password':self.api_pass},'id':0}
@@ -17,9 +29,38 @@ class ZabbixApi:
 		request.add_header('Content-Type','application/json')
 		response = urllib2.urlopen(request)
 		var1 = json.loads(response.read())
-		self.session =  var1['result']
-		print self.session
+		self.auth =  var1['result']
+		print self.auth
+		self.id += 1
 
+	def getproxy(self):
+##json data
+		data = {
+    "jsonrpc": "2.0",
+    "method": "proxy.get",
+    "params": {
+        "output": "extend",
+        "selectInterface": "extend"
+    },
+    "auth": self.auth,
+    "id": self.id}
+## end data
+		request = urllib2.Request(self.zabbix_url,json.dumps( data ))		
+		request.add_header('Content-Type','application/json')
+		response = urllib2.urlopen(request)
+		var1 = json.loads(response.read())
+	#	print var1['result']
+		for p in var1['result']:
+			theid = p['proxyid']
+			thehost = p['host']
+			thestatus =  p['status']
+			n1 = Proxy( theid,thehost,thestatus )
+			self.proxylist.append(n1)
+		self.id += 1
+	def printproxy(self):
+		for i in self.proxylist:
+			i.say()
+		
 
 if __name__ == '__main__':
 	with open('.config') as f:
@@ -27,3 +68,6 @@ if __name__ == '__main__':
 		api_pass = f.readline().strip()
 	test = ZabbixApi(api_user,api_pass)
 	test.auth()
+	test.getproxy()
+	test.printproxy()
+
