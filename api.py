c9e683dfa6ba51748ad8514ac828cd9c31bb6a65
gqdw <aca_jingru@qq.com>
2015-09-16 13:32:16 +0800

    with config file '.config'

--- api.py
+++ api.py
@@ -3,9 +3,9 @@ import json
 import sys
 zabbix_url="http://zabbix.jiagouyun.com/api_jsonrpc.php"
 
-api_user = sys.argv[1]
-api_pass = sys.argv[2]
-	
+with open('.config') as f:
+	api_user = f.readline().strip()
+	api_pass = f.readline().strip()
 auth_data={ 'jsonrpc':'2.0','method':'user.login','params':{'user':api_user,'password':api_pass},'id':0}
 def get_auth():
 	request=urllib2.Request(zabbix_url,json.dumps(auth_data))
