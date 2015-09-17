4eea187eba649fc3817fc5f1f7442b4759f36a0f
gqdw <aca_jingru@qq.com>
2015-09-17 10:12:33 +0800

    add form to views

--- zfront/main/views.py
+++ zfront/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
 
 # Create your views here.
+class Addbox(forms.Form):
+	host = forms.CharField(max_length=30)
+	ip = forms.GenericIPAddressField()
+
+def index( request ):
+	f = Addbox()
+	return render(request ,'index.html',{'form':f})
+#	return HttpResponse('ok')
